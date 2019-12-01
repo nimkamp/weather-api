7ce8d1229158ae6afd3b49d8ff5c3095426879fa
Nick Imkamp <nick.imkamp@fuseintegration.com>
2019-11-30 18:31:28 -0800

    Move unit tests to project folder. Remove src directory

--- api/weather/test/test_getcurrentaveragetemperature.py
+++ api/weather/test/test_getcurrentaveragetemperature.py
@@ -1,21 +1,15 @@
 import unittest
 import sys
-sys.path.append('../src')
-from weatheraverage import getCurrentAverageTemperature
+sys.path.append('../')
+from views import getCurrentAverageTemperature
 
 
 class TestGetCurrentAverageTemperature(unittest.TestCase):
 
-    def setUp(self):
-        pass
-
-    def test(self):
-        self.assertTrue(True)
-
-    def test_1_weather_average(self):
+    def test_1_get_current_average_temperature(self):
         self.assertEqual(getCurrentAverageTemperature(44, 33, ["noaa", "accuweather", "weatherdotcom"]), 49)
 
-    def test_2_weather_average(self):
+    def test_2_get_current_average_temperature(self):
         self.assertEqual(getCurrentAverageTemperature(44, 33, ["noaa", "accuweather"]), 55)
 
 if __name__ == '__main__': 
